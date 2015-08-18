e0173ba19a5c7978d4bd7b183121bb7b42e05db1
minutephp <dev@minutephp.com>
2015-08-18 21:49:40 +0530

    v1

---
+++ src/js/src/js/angular-datatable-links.js
@@ -0,0 +1,23 @@
+(function () {
+    'use strict';
+
+    angular.module('angularDatatableLinks', ['session'])
+        .directive('ngTrLink', ['$timeout', '$session', function ($timeout, $session) {
+            return {
+                restrict: 'A',
+                scope: {ngTrLink: '='},
+                link: function ($scope, element, attrs) {
+                    element.mousedown($scope.rowClick);
+                },
+                controller: function ($scope, $element, $session) {
+                    $scope.rowClick = function () {
+                        if (!$(event.target).closest('td.unclickable').length) {
+                            top.location.href = /^\//.test($scope.ngTrLink) ? $scope.ngTrLink : $session.request.uri + '/' + $scope.ngTrLink;
+                        }
+                    };
+                }
+            };
+        }]);
+})();
+
+
